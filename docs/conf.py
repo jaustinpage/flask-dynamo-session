9acd64cc563c467676ccc1eba205dcd2a496f4eb
Austin Page <jaustinpage@gmail.com>
2017-08-16 20:16:46 -0500

    Adding badges, py2 and py3

--- docs/conf.py
+++ docs/conf.py
@@ -20,9 +20,10 @@ import os
 import sys
 import sphinx_rtd_theme
 
-from flask_dynamo_session.__version__ import __version__
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+from flask_dynamo_session.__version__ import __version__
 
 # -- General configuration ------------------------------------------------
 
